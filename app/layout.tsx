bb947dfc92ef1e8cc6035fcf9f351e0ea17dd83e
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-02 09:56:20 -0500

    Bring in image assets and update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import { ReactElement } from "react";
 import "@/assets/styles/globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Property TEK Sphere",
+  keywords: "rental, property, real estate",
+  description:
+    "Property management portal. Find rental, for sale, and other types of property.",
+};
 
 const MainLayout = ({ children }: { children: ReactElement }) => {
   return (
