f6dc43ccf3145f8b14b2e3644b49255486324f52
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-03 21:29:46 -0500

    Bring in mogo db packages and connect to db

---
+++ custom.d.ts
@@ -0,0 +1,9 @@
+declare var process: {
+  env: {
+    MONGO_USER: string;
+    MONGO_PASS: string;
+    MONGODB_URI: string;
+    NEXT_PUBLIC_DOMAIN: string;
+    NEXT_PUBLIC_API_DOMAIN: string;
+  };
+};
