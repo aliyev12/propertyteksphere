3a5be9f7732ae81849c620d6ba5e86b9e90563bf
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-05 06:49:13 -0500

    Sign up for google OAuth and add credentials along with next auth to env vars

--- custom.d.ts
+++ custom.d.ts
@@ -5,5 +5,10 @@ declare var process: {
     MONGODB_URI: string;
     NEXT_PUBLIC_DOMAIN: string;
     NEXT_PUBLIC_API_DOMAIN: string;
+    GOOGLE_CLIENT_ID: string;
+    GOOGLE_CLIENT_SECRET: string;
+    NEXTAUTH_URL: string;
+    NEXTAUTH_URL_INTERNAL: string;
+    NEXTAUTH_SECRET: string;
   };
 };
