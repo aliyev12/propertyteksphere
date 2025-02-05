e3ab3de3328d5e89c44f93aae630ba857294631b
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-04 22:51:04 -0500

    Create property details page

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -4,6 +4,7 @@ import PropertyHeaderImage from "@/components/property-header-image";
 import { IProperty } from "@/types/property.types";
 import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
+import PropertyDetails from "@/components/property-details";
 
 interface IPropertyPageProps {
   params: Promise<{ id: string }>;
@@ -28,7 +29,7 @@ const PropertyPage = async ({ params }: IPropertyPageProps) => {
       <section className="">
         <div className="container m-auto  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
-            {/* {"Property Info"} */}
+            <PropertyDetails property={property} />
           </div>
         </div>
       </section>
