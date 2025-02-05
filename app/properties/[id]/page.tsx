71815cf9ce465f8dd817ab3cb9a14a9995e49789
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-04 20:06:22 -0500

    Add property header image component

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -1,11 +1,39 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+import PropertyHeaderImage from "@/components/property-header-image";
+import { IProperty } from "@/types/property.types";
+import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
+
 interface IPropertyPageProps {
   params: Promise<{ id: string }>;
 }
 
 const PropertyPage = async ({ params }: IPropertyPageProps) => {
+  await connectDB();
   const id = (await params).id;
+  const property = (await Property.findById(id).lean()) as unknown as IProperty;
 
-  return <div>property page: {id}</div>;
+  return (
+    <div className="space-y-10 lg:space-y-16">
+      <PropertyHeaderImage image={property.images[0]} />
+      <section>
+        <div className="container m-auto  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <Link href="/properties" className="flex  items-center">
+            <ArrowLeft className="mr-2" size={18} /> Back to Properties
+          </Link>
+        </div>
+      </section>
+
+      <section className="">
+        <div className="container m-auto  max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
+            {/* {"Property Info"} */}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default PropertyPage;
