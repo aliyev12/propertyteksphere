38cc7f374e9ab09ee8c6ff82f4acc23a6557f229
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-19 20:52:25 -0500

    Add ability to mark messages read or new

--- components/message-card.tsx
+++ components/message-card.tsx
@@ -1,11 +1,30 @@
+"use client";
 import { IMessagePopulated } from "@/app/(root)/messages/page";
 import { IMessage } from "@/models/Message";
 import { Button } from "./ui/button";
 import { Check, Trash } from "lucide-react";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import markMessageAsRead from "@/app/actions/mark-message-as-read";
+import { Badge } from "./ui/badge";
 
 const MessageCard = ({ message }: { message: IMessagePopulated }) => {
+  const [isRead, setIsRead] = useState(message.read);
+
+  async function handleReadClick() {
+    const read = await markMessageAsRead(message._id);
+
+    setIsRead(read);
+    toast.success(`Marked as ${read ? "read" : "new"}`);
+  }
+
   return (
     <div className="relative p-4 rounded-md shadow-md border">
+      {!isRead && (
+        <Badge variant="secondary" className="absolute top-2 right-2">
+          New
+        </Badge>
+      )}
       <h2 className="text-xl mb-4">
         <span className="font-bold">Property Inquiry:</span>{" "}
         {message.property.name}
@@ -31,8 +50,8 @@ const MessageCard = ({ message }: { message: IMessagePopulated }) => {
         </li>
       </ul>
       <div className="flex flex-col space-y-3 mt-4 sm:space-y-0 sm:space-x-3 sm:flex-row sm:items-center">
-        <Button>
-          <Check /> Mark as read
+        <Button onClick={handleReadClick}>
+          <Check /> {isRead ? "Mark as new" : "Mark as read"}
         </Button>
         <Button variant="destructive">
           <Trash /> Delete
