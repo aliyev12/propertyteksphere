26a5873957566acf596af00f5ac1684b8a882088
Abdul Aliyev <aaa7c4@gmail.com>
2025-02-20 20:36:13 -0500

    Add images photoswipe to property details pages

--- components/property-images.tsx
+++ components/property-images.tsx
@@ -1,43 +1,69 @@
+"use client";
 import Image from "next/image";
+import { Gallery, Item } from "react-photoswipe-gallery";
 
 const PropertyImages = ({ images }: { images: string[] }) => {
   if (!images || !Array.isArray(images) || !images.length) return null;
 
   return (
-    <section className="">
-      <div className="container mx-auto px-4 sm:px-6 lg:px-8">
-        {images.length === 1 ? (
-          <Image
-            src={images[0]}
-            alt="Main Property Image"
-            className="object-cover h-[400px] mx-auto rounded-xl"
-            width={1800}
-            height={400}
-            priority={true}
-          />
-        ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            {images.map((image, i) => (
-              <div
-                key={i}
-                className={`${
-                  images.length === 3 && i === 2 ? "col-span-2" : "col-span-1"
-                }`}
-              >
+    <Gallery>
+      <section className="">
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8">
+          {images.length === 1 ? (
+            <Item
+              original={images[0]}
+              thumbnail={images[0]}
+              width={1000}
+              height={600}
+            >
+              {({ ref, open }) => (
                 <Image
-                  src={image}
-                  alt={`Property Image ${i + 1}`}
-                  className="object-cover h-[400px] w-full rounded-xl"
+                  src={images[0]}
+                  ref={ref}
+                  onClick={open}
+                  alt="Main Property Image"
+                  className="cursor-pointer object-cover h-[400px] mx-auto rounded-xl"
                   width={1800}
                   height={400}
                   priority={true}
                 />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
-    </section>
+              )}
+            </Item>
+          ) : (
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+              {images.map((image, i) => (
+                <div
+                  key={i}
+                  className={`${
+                    images.length === 3 && i === 2 ? "col-span-2" : "col-span-1"
+                  }`}
+                >
+                  <Item
+                    original={image}
+                    thumbnail={image}
+                    width={1000}
+                    height={600}
+                  >
+                    {({ ref, open }) => (
+                      <Image
+                        src={image}
+                        ref={ref}
+                        onClick={open}
+                        alt={`Property Image ${i + 1}`}
+                        className="cursor-pointer object-cover h-[400px] w-full rounded-xl"
+                        width={1800}
+                        height={400}
+                        priority={true}
+                      />
+                    )}
+                  </Item>
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      </section>
+    </Gallery>
   );
 };
 
