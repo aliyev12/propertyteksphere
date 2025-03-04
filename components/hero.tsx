dbb3642ff83d385da951c0f802679ec0be09a92a
Abdul Aliyev <aaa7c4@gmail.com>
2025-03-03 20:01:51 -0500

    Remove comments

--- components/hero.tsx
+++ components/hero.tsx
@@ -20,7 +20,6 @@ const Hero = () => {
       style={{ backgroundImage: "url('/images/hero-img.jpg')" }}
     >
       <div className="absolute inset-0 bg-black/50"></div>{" "}
-      {/* Dark overlay for better readability */}
       <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
         <div className="text-center">
           <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
